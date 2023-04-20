80c5addb23af1dce7211da31910be6a6e2919764
marahmanjs <imarahmandev@gmail.com>
2023-04-20 15:38:25 +0500

    format string

---
+++ formatString.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func formatString() {
+	var name string = "Adam"
+	var age float32 = 12.50
+
+	fmt.Println("My Name is", name, "and i am", age, "years old!")
+	fmt.Printf("My Name is %q and i am %0.1f years old \n", name, age)
+
+	var message string = fmt.Sprintf("My Name is %q and i am %0.1f years old", name, age)
+
+	fmt.Println(message)
+}
