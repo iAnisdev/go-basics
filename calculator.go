a50b319c7d32cdb74128e36cecbc32eb110ce4a1
marahmanjs <imarahmandev@gmail.com>
2023-05-02 19:22:34 +0500

    function usage

---
+++ calculator.go
@@ -0,0 +1,21 @@
+package main
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divide(a int, b int) int {
+	return a / b
+}
+
+func reminder(a int, b int) int {
+	return a % b
+}
--- hello.go
+++ hello.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	Conditions()
+	fmt.Println(add(4, 7))
+	fmt.Println(sub(19, 3))
+	fmt.Println(multiply(5, 9))
+	fmt.Println(divide(18, 3))
 }
