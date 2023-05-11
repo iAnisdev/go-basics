4db4553e2b79cec7645e9566f48634af9024c0e4
Anis <anis.rahman@nfq.com>
2023-05-11 10:58:29 +0700

    Maps

--- maps.go
+++ maps.go
@@ -12,6 +12,16 @@ func Maps() {
 	fmt.Println(user["name"])
 	user["name"] = "GHI"
 	fmt.Println(user["name"])
+	user["Address"] = "JKL"
+	fmt.Println(user)
+	delete(user, "age")
+	fmt.Println(user)
+
+	name, isNameAvailable := user["name"]
+	if isNameAvailable {
+		fmt.Println(name)
+	}
+	fmt.Println(len(user))
 
 	menu := map[string]int{
 		"Rice":  12,
